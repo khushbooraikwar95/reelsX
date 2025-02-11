60a8c05578cfb4358a034c20ecd253a257780889
khushbooraikwar95 <j.khushbooraikwar95@gmail.com>
2025-02-11 20:43:07 +0530

    Imagekit configuration done for backend

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["ik.imagekit.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
+      },
+    ],
   },
 };
 
