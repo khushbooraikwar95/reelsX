e27680dc297d37ec4092d24e69ea4dcb15174dca
khushbooraikwar95 <j.khushbooraikwar95@gmail.com>
2025-02-12 21:32:55 +0530

    Front-end pages implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import Providers from "./components/Providers";
 import "./globals.css";
 import { Inter } from "next/font/google";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -16,7 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="container mx-auto px-4 py-8">{children}</main>
+        <Providers>
+          <Header />
+          <main className="container mx-auto px-4 py-8">{children}</main>
+        </Providers>
       </body>
     </html>
   );
